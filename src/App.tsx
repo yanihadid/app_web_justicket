050ea125a6ac8ee4ef273d6fe66890e33a0a47dc
hadid <yanis.hadid.etu@univ-lille.fr>
2025-03-04 16:42:47 +0100

    ajout footer en bas

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
+import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from '@mui/material'; 
 import ConcertList from "./presentation/pages/ConcertList";
 import MesBillets from "./presentation/pages/MesBillets";
 import Navbar from "./presentation/components/Navbar";
@@ -9,17 +11,21 @@ import Footer from "./presentation/components/Footer";
 
 function App() {
   return (
-    <BrowserRouter>
-     <Navbar />
-      <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/concerts" element={<ConcertList />} />
-          <Route path="/billets" element={<MesBillets />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
-      </Routes>
-      <Footer/>
-    </BrowserRouter>
+    <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+      <BrowserRouter>
+        <Navbar />
+        <Box sx={{ flex: '1 0 auto' }}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/concerts" element={<ConcertList />} />
+            <Route path="/billets" element={<MesBillets />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+          </Routes>
+        </Box>
+        <Footer />
+      </BrowserRouter>
+    </Box>
   );
 }
 
--- src/presentation/components/Footer.tsx
+++ src/presentation/components/Footer.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Typography, Container } from '@mui/material';
+import { Box, Typography, Container, Link } from '@mui/material';
 
 const Footer: React.FC = () => {
   return (
@@ -10,12 +10,26 @@ const Footer: React.FC = () => {
         py: 3,
         mt: 'auto',
         textAlign: 'center',
+        position: 'relative',
+        bottom: 0,
+        width: '100%',
       }}
     >
       <Container>
         <Typography variant="body1">
           &copy; {new Date().getFullYear()} Justicket. All rights reserved.
         </Typography>
+        <Box sx={{ mt: 2 }}>
+          <Link href="/about" color="inherit" sx={{ mx: 1 }}>
+            à propos
+          </Link>
+          <Link href="/contact" color="inherit" sx={{ mx: 1 }}>
+            Contact
+          </Link>
+          <Link href="/privacy" color="inherit" sx={{ mx: 1 }}>
+            Privacy Policy
+          </Link>
+        </Box>
       </Container>
     </Box>
   );
